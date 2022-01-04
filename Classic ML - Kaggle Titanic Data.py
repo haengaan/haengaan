91507878388cc7bdc0ee64a2bac386f3ce9a6751
haengaan <97069410+haengaan@users.noreply.github.com>
2022-01-04 09:42:43 +0900

    Update Classic ML - Kaggle Titanic Data.py

--- Classic ML - Kaggle Titanic Data.py
+++ Classic ML - Kaggle Titanic Data.py
@@ -1,7 +1,8 @@
 '''
 Titanic 생존자 예측
 타이타닉 호 침몰 사건 당시의 사망자와 생존자를 구분하는 요인 분석을 통해, 승객들의 생존 여부를 예측
-https://computer-science-student.tistory.com/113
+code source: https://computer-science-student.tistory.com/113
+data source: https://www.kaggle.com/c/titanic/data
 '''
 
 # 기본 라이브러리
